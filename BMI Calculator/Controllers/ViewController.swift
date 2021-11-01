aa428c8d5a98421f53ee210dee10416cdfa3251c
Nikolai Kharkevich <niko.kharkevych@gmail.com>
2021-11-01 20:25:13 +0200

    added BMI logic to Calculate button

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -17,6 +17,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
     
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    
     @IBAction func heightSliderChanged(_ sender: UISlider) {
         heightLabel.text = (String(format: "%.2f", sender.value) + "m")
     }
@@ -26,5 +30,13 @@ class ViewController: UIViewController {
         weightLabel.text = (String(format: "%.0f", sender.value) + "Kg")
     }
     
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = weight / pow(height, 2)
+        print(bmi)
+    }
+
 }
 
