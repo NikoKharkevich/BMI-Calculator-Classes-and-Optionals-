7115d747afb7dba7be2771b60298a03d1863ac37
Nikolai Kharkevich <niko.kharkevych@gmail.com>
2021-11-23 18:34:21 +0200

    adding logic to SecondVC with calculated BMI

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -27,7 +27,13 @@ class ViewController: UIViewController {
         let height = heightSlider.value
         let weight = weightSlider.value
         let bmi = weight / pow(height, 2)
-        print(bmi)
+
+        let secondVC = SecondViewController()
+        
+//        formatting float bmit to string bmit with 1 number after .
+        secondVC.bmiValue = String(format: "%.1f", bmi)
+        
+        self.present(secondVC, animated: true, completion: nil)
     }
 
 }
